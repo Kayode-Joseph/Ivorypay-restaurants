ea2bf3945755642baa45d2f7324fe7b29b8e6a39
kayode.joseph.1252@gmail.com <jasonola120@yahoo.com>
2024-04-29 14:39:48 +0100

    use global validator

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -14,9 +14,9 @@ import { RestaurantResponse } from './dto/commonRestaurant';
 import { CreateRestaurantDto } from './dto/createRestaurant';
 import { RestaurantsService } from './restaurants.service';
 import { RestaurantNamePathVariable } from './dto/commonRestaurant';
-import { pipe } from 'rxjs';
 import { UpdateRestaurantRequest } from './dto/updateRestaurant';
 
+//every request here is validated by a global valdiator set in the src/main.ts
 @Controller('/v1/restaurants')
 export class RestaurantsController {
   constructor(private readonly restaurantService: RestaurantsService) {}
@@ -25,13 +25,7 @@ export class RestaurantsController {
   //using async because calulating the logitude / latitude range in the service layer in an expensive operation
   //we dont want to block the http thread
   async getRestaurants(
-    @Query(
-      new ValidationPipe({
-        transform: true,
-        transformOptions: { enableImplicitConversion: true },
-        forbidNonWhitelisted: true,
-      }),
-    )
+    @Query()
     getRestaurantsParam: GetRestaurantQueryParams,
   ): Promise<RestaurantResponse[]> {
     //the result would already be sorted by RestaurantServiceModel#orderScore, in descending order
@@ -41,13 +35,7 @@ export class RestaurantsController {
 
   @Post()
   async createRestaurant(
-    @Body(
-      new ValidationPipe({
-        transform: true,
-        transformOptions: { enableImplicitConversion: true },
-        forbidNonWhitelisted: true,
-      }),
-    )
+    @Body()
     createRestaurant: CreateRestaurantDto,
   ): Promise<CreateRestaurantDto> {
     return await this.restaurantService.createRestuarants(createRestaurant);
@@ -57,33 +45,23 @@ export class RestaurantsController {
   //todo: use global pipes
   @Delete(':name')
   async deleteRestaurant(
-    @Param(
-      new ValidationPipe({
-        transform: true,
-        transformOptions: { enableImplicitConversion: true },
-        forbidNonWhitelisted: true,
-      }),
-    )
+    @Param()
     nameObject: RestaurantNamePathVariable,
   ): Promise<RestaurantResponse> {
     return await this.restaurantService.deleteRestaurant(nameObject.name);
   }
 
-  @Put('name')
+  @Put(':name')
   async updateRestaurant(
-    @Param(
-      new ValidationPipe({
-        transform: true,
-        transformOptions: { enableImplicitConversion: true },
-        forbidNonWhitelisted: true,
-      }),
-    )
+    @Param()
     nameObject: RestaurantNamePathVariable,
     @Body(
       new ValidationPipe({
         transform: true,
         transformOptions: { enableImplicitConversion: true },
         forbidNonWhitelisted: true,
+        skipMissingProperties: true, 
+        //we want to skip missing properties here becuause you can decide to only set only some proprty
       }),
     )
     updateRestaurant: UpdateRestaurantRequest,
