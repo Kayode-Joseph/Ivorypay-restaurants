1ac13e51e2e79375d406878d927aa5703334b101
kayode.joseph.1252@gmail.com <jasonola120@yahoo.com>
2024-04-29 11:37:54 +0100

    sock questiin solution

---
+++ SOCK_ALGORITHM_SOLUTION.ts
@@ -0,0 +1,34 @@
+function calcualateNumberOfSocksWithCompletePair(
+  numberOfSocks: number,
+  arrayOfSocksInThePile: number[],
+): number {
+  const socksInthePileMap = new Map<number, number>();
+
+  let numberOfCompletePairs = 0;
+
+  for (let i = 0; i < arrayOfSocksInThePile.length; i++) {
+    const currentSock: number = arrayOfSocksInThePile[i];
+
+    //check if we have seen an unrecorded sock in the map before
+    const numberOfSockFoundPerColor: number | undefined =
+      socksInthePileMap.get(currentSock);
+
+    //if we have not seen an unrecored sock,
+    //we want to record this sock in the map, and move to the next sock
+    if (!numberOfSockFoundPerColor) {
+      socksInthePileMap.set(currentSock, 1);
+      continue;
+    }
+
+    //if we've seen the sock before, we now have the
+    // complete 2 pair of the sock of this color, so we increase the sock count
+    //and delete this map entry,
+    //so the next sock of the same color can be recorded and its pair found or not found
+    numberOfCompletePairs += 1;
+    socksInthePileMap.delete(currentSock);
+  }
+
+  return numberOfCompletePairs;
+}
+
+export default calcualateNumberOfSocksWithCompletePair;
